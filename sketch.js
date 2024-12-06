41fe4bc831b217b0ccc401634b4ea828f406260d
Evann (lw) <--remove>
2024-12-06 11:51:50 +0100

    cleanup examples

--- sketch.js
+++ sketch.js
@@ -41,9 +41,6 @@ function setup() {
 function draw() {
   background(color(Settings.COLOR_BACKGROUND));
 
-  // Set angle based on frameCount, and display current value
-  //let angle = frameCount % 360;
-
   {//texts
     fill(255);
     textSize(20);
@@ -63,92 +60,3 @@ function draw() {
 
   PERSPECTIVE_BY(grid);
 }
-/*
-  // Draw moving points
-
-  let pointX = Scale.x(circleX) + Scale.x(circleRadius) * cos(angle);
-  let pointY = Scale.y(circleY) - Scale.x(circleRadius) * sin(angle);
-
-  line(Scale.x(circleX), Scale.y(circleY), pointX, pointY);
-
-  noStroke();
-
-  fill('white');
-  circle(pointX, pointY, 10);
-
-  fill('orange');
-  circle(pointX, Scale.y(circleY), 10);
-
-  fill('red');
-  circle(Scale.x(circleX), pointY, 10);
-
-  // Draw graph
-
-  stroke('grey');
-  strokeWeight(3);
-  line(Scale.x(graphX), Scale.y(graphY), Scale.x(graphX) + 300, Scale.y(graphY));
-  line(Scale.x(graphX), Scale.y(graphY) - Scale.y(graphAmplitude), Scale.x(graphX), Scale.y(graphY) + Scale.y(graphAmplitude));
-  line(
-    Scale.x(graphX) + Scale.x(graphPeriod),
-    Scale.y(graphY) - Scale.y(graphAmplitude),
-    Scale.x(graphX) + Scale.x(graphPeriod),
-    Scale.y(graphY) + Scale.y(graphAmplitude)
-  );
-
-  fill('grey');
-  strokeWeight(1);
-  textAlign(CENTER, CENTER);
-  text('0', Scale.x(graphX), Scale.y(graphY) + Scale.y(graphAmplitude) + 20);
-  text('360', Scale.x(graphX) + Scale.x(graphPeriod), Scale.y(graphY) + Scale.y(graphAmplitude) + 20);
-  text('1', Scale.x(graphX) / 2, Scale.y(graphY) - Scale.y(graphAmplitude));
-  text('0', Scale.x(graphX) / 2, Scale.y(graphY));
-  text('-1', Scale.x(graphX) / 2, Scale.y(graphY) + Scale.y(graphAmplitude));
-
-  fill('orange');
-  text('cos', Scale.x(graphX) + Scale.x(graphPeriod) + Scale.x(graphX) / 2, Scale.y(graphY) - Scale.y(graphAmplitude));
-  fill('red');
-  text('sin', Scale.x(graphX) + Scale.x(graphPeriod) + Scale.x(graphX) / 2, Scale.y(graphY));
-
-  // Draw cosine curve
-
-  noFill();
-  stroke('orange');
-  beginShape();
-  for (let t = 0; t <= 360; t++) {
-    let x = map(t, 0, 360, Scale.x(graphX), Scale.x(graphX) + Scale.x(graphPeriod));
-    let y = Scale.y(graphY) - Scale.y(graphAmplitude) * cos(t);
-    vertex(x, y);
-  }
-  endShape();
-
-  // Draw sine curve
-
-  noFill();
-  stroke('red');
-  beginShape();
-  for (let t = 0; t <= 360; t++) {
-    let x = map(t, 0, 360, Scale.x(graphX), Scale.x(graphX) + Scale.x(graphPeriod));
-    let y = Scale.y(graphY) - Scale.y(graphAmplitude) * sin(t);
-    vertex(x, y);
-  }
-  endShape();
-
-  // Draw moving line
-
-  let lineX = map(angle, 0, 360, Scale.x(graphX), Scale.x(graphX) + Scale.x(graphPeriod));
-  stroke('grey');
-  line(lineX, Scale.y(graphY) - Scale.y(graphAmplitude), lineX, Scale.y(graphY) + Scale.y(graphAmplitude));
-
-  // Draw moving points on graph
-
-  let orangeY = Scale.y(graphY) - Scale.y(graphAmplitude) * cos(angle);
-  let redY = Scale.y(graphY) - Scale.y(graphAmplitude) * sin(angle);
-
-  noStroke();
-
-  fill('orange');
-  circle(lineX, orangeY, 10);
-
-  fill('red');
-  circle(lineX, redY, 10);
-*/
