2df3c0786d9710a8f5b84846aebcea845a9f0bd2
Evann (lw) <--remove>
2024-12-07 19:39:29 +0100

    using MatrixTranslation object but work weird

--- src/dimensionalDependent/translation.js
+++ src/dimensionalDependent/translation.js
@@ -38,10 +38,8 @@ function translation_draw_3D_cube(grid)
 			}
 
 			//find matrix
-			const translationMatrix=matrix_3D_translations[moveKey];
-			if (!translationMatrix)
-				return;
-			//console.log(`move with matrix: ${translationMatrix[0]}`);
+			const translationObject=matrix_3D_translations[moveKey];
+			//console.log(`move with matrix: ${translationObject.matrix}`);
 
 			//do the translation
 		  for (let posKey of game.grid.map_keys)
@@ -50,27 +48,15 @@ function translation_draw_3D_cube(grid)
 				for (let i=0;i<h_box.morph.get_quadri_amount();i+=1)
 				{
 					let quadri = h_box.morph.get_quadri(i);
-					quadri=quadri.map(pos => translation_key_3D_cube_translate(pos, translationMatrix, power, grid.center));
+					quadri=quadri.map(pos => translationObject.translate(pos, power, grid.center));
 					h_box.morph.set_quadri(i, quadri);
 				}
 			}
-			grid.center=translation_key_3D_cube_translate(grid.center, translationMatrix, power);
+			if (translationObject[1])
+			console.log("move to center:",grid.center);
+			grid.center=translationObject.translate(grid.center, power, grid.center);
 		}
 	}
-}
-function translation_key_3D_cube_translate(pos, translationMatrix, power, center)
-{
-	let posMatrix=new Matrix(4, 1).set_column(0, [pos[0], pos[1], pos[2], power]);
-	if (translationMatrix[1])
-	{//move origin to center
-		posMatrix=matrix_identity.copy().set_column(3, [-center[0], -center[1], -center[2], 1]).multiply_with(posMatrix);
-	}
-	posMatrix=translationMatrix[0].multiply_with(posMatrix);
-	if (translationMatrix[1])
-	{//move back
-		posMatrix=matrix_identity.copy().set_column(3, [center[0], center[1], center[2], 1]).multiply_with(posMatrix);
-	}
-	return posMatrix.data.slice(0,3);
 }
 
 
@@ -92,17 +78,50 @@ const key_to_move = {
 	88:"y>",
 	67:"z>",
 }
-const matrix_3D_translations = {
-	"x-": [matrix_identity.copy().set_column(3, [-matrix_strength,0,0,1]),false],
-	"x+": [matrix_identity.copy().set_column(3, [matrix_strength,0,0,1]),false],
-	
-	"y-": [matrix_identity.copy().set_column(3, [0,0,matrix_strength,1]),false],
-	"y+": [matrix_identity.copy().set_column(3, [0,0,-matrix_strength,1]),false],
-	
-	"z-": [matrix_identity.copy().set_column(3, [0,-matrix_strength,0,1]),false],
-	"z+": [matrix_identity.copy().set_column(3, [0,matrix_strength,0,1]),false],
+class MatrixTranslation {
+	constructor(matrix, if_aroundCenter=false, if_powerInPos=false, if_powerCalibre=false, power_calibration= (matrix, power) => matrix)
+	{
+		this.matrix=matrix;
+		this.center_around=if_aroundCenter;
+		this.power_calibrate=if_powerCalibre;
+		this.power_isLastPos=if_powerInPos;
+		this.calibration=power_calibration;
+	}
+	translate(pos, power, center)
+	{
+		let posMatrix=new Matrix(4, 1).set_column(0, [pos[0], pos[1], pos[2], (this.power_isLastPos) ? power : 1]);
+		
+		if (this.center_around)
+		{//move origin to center
+			posMatrix=matrix_identity.copy().set_column(3, [-center[0], -center[1], -center[2], 1]).multiply_with(posMatrix);
+		}
+		
+		if (this.power_calibrate)
+		{//redefine matrix itself. modification must redefine same points each time.
+			this.matrix=this.calibration(this.matrix, power);
+		}
 
-	"x>": [matrix_identity.copy().set_at(1,1,Math.cos(matrix_angle)).set_at(2,2,Math.cos(matrix_angle)).set_at(1,2,Math.sin(matrix_angle)).set_at(2,1,-Math.sin(matrix_angle)),true],
-	"y>": [matrix_identity.copy().set_at(0,0,Math.cos(matrix_angle)).set_at(2,2,Math.cos(matrix_angle)).set_at(0,2,-Math.sin(matrix_angle)).set_at(2,0,Math.sin(matrix_angle)),true],
-	"z>": [matrix_identity.copy().set_at(0,0,Math.cos(matrix_angle)).set_at(1,1,Math.cos(matrix_angle)).set_at(0,1,Math.sin(matrix_angle)).set_at(1,0,-Math.sin(matrix_angle)),true]
+		posMatrix=this.matrix.multiply_with(posMatrix);
+		
+		if (this.center_around)
+		{//move back
+			posMatrix=matrix_identity.copy().set_column(3, [center[0], center[1], center[2], 1]).multiply_with(posMatrix);
+		}
+
+		return posMatrix.data.slice(0,3);//return as a point
+	}
+};
+const matrix_3D_translations = {
+	"x-": new MatrixTranslation(matrix_identity.copy().set_column(3, [-matrix_strength,0,0,1]), false, true, false),
+	"x+": new MatrixTranslation(matrix_identity.copy().set_column(3, [matrix_strength,0,0,1]), false, true, false),
+	
+	"y-": new MatrixTranslation(matrix_identity.copy().set_column(3, [0,0,matrix_strength,1]), false, true, false),
+	"y+": new MatrixTranslation(matrix_identity.copy().set_column(3, [0,0,-matrix_strength,1]), false, true, false),
+	
+	"z-": new MatrixTranslation(matrix_identity.copy().set_column(3, [0,-matrix_strength,0,1]), false, true, false),
+	"z+": new MatrixTranslation(matrix_identity.copy().set_column(3, [0,matrix_strength,0,1]), false, true, false),
+
+	"x>": new MatrixTranslation(matrix_identity.copy(), true, false, true, (matrix, power) => matrix.set_at(1,1,Math.cos(matrix_angle)).set_at(2,2,Math.cos(matrix_angle*power)).set_at(1,2,Math.sin(matrix_angle*power)).set_at(2,1,-Math.sin(matrix_angle*power))),
+	"y>": new MatrixTranslation(matrix_identity.copy(), true, false, true, (matrix, power) => matrix.set_at(0,0,Math.cos(matrix_angle)).set_at(2,2,Math.cos(matrix_angle*power)).set_at(0,2,-Math.sin(matrix_angle*power)).set_at(2,0,Math.sin(matrix_angle*power))),
+	"z>": new MatrixTranslation(matrix_identity.copy(), true, false, true, (matrix, power) => matrix.set_at(0,0,Math.cos(matrix_angle)).set_at(1,1,Math.cos(matrix_angle*power)).set_at(0,1,Math.sin(matrix_angle*power)).set_at(1,0,-Math.sin(matrix_angle*power)))
 }
